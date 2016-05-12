34052da086a832b7b83bc1e0ec4546103e0cda2a
Sajjad Hossain <my.lost.word@gmail.com>
2016-05-12 21:55:51 +0600

    #1 - Add demo-background in test scene to adopt different screen resolutions

---
+++ Assets/scripts/ScreenAdjust.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenAdjust : MonoBehaviour {
+	public float SCREEN_WIDTH = 1080f;
+	public float SCREEN_HEIGHT = 1920f;
+
+	// Use this for initialization
+	void Start () {
+		Camera.main.aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+	}
+}
